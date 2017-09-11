23bdf81a7c03cde21bb2823d48ec614769443f92
kripesh123 <kripeshbista3@gmail.com>
2017-09-11 11:37:35 +0545

    added user role and email. Added email field in register form. Added link to redirect user after login to My Album.

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,11 +1,14 @@
 import { Photo } from './photo';
+import {Role} from './role';
 
 export class User {
   public userId: number;
   public firstName: string;
   public lastName: string;
+  public email: string;
   public userName: string;
   public password: string;
+  public role: Set<Role>;
   public created: Date;
   public photoList : Photo[];
   public likedPhotoList: Photo[];
